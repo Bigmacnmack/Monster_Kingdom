74216c3dfd7c2ed8762b9f327e4f9227353554a0
Bigmacnmack <95834166+Bigmacnmack@users.noreply.github.com>
2022-04-29 19:16:59 -0700

    Delete Monster_Kingdom.cpp

--- Monster_Kingdom.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Monster_Kingdom.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Monster_Kingdom, "Monster_Kingdom" );
